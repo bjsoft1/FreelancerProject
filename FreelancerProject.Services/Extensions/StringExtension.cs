0950631b2af9a9380073d76829b475b35e4b5cd8
bijay adhikari <bijay.adhikari.9674227@gmail.com>
2023-10-07 16:47:07 +0545

    Product Lising, Basic Filter System Maked

---
+++ FreelancerProject.Services/Extensions/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreelancerProject.Services.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return (value == null || value.Trim() == string.Empty);
+        }
+    }
+}
--- FreelancerProject.Web/Areas/Product/ProductController.cs
+++ FreelancerProject.Web/Areas/Product/ProductController.cs
@@ -1,14 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreelancerProject.Services.DbContexts;
+using FreelancerProject.Services.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FreelancerProject.Web.Areas.Product
 {
     [Area("Product")]
     public class ProductController : Controller
     {
+        private readonly ProductDbContext _dbContext;
+        public ProductController(ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         [HttpGet("/Product")]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet("/Product/GetProductPaginationListing")]
+        public async Task<IActionResult> GetProductPaginationListing(int skipCount = 0, int maxCount = 10, string SearchKeyword = "")
+        {
+            try
+            {
+                // Create Product Query For Make similary data comes in Counter and Data List.
+                var productQuery = _dbContext.Products.Where(x => !x.IsDeleted && x.IsActive);
+
+                //Basic Filter
+                if (!SearchKeyword.IsNullOrWhiteSpace())
+                    productQuery = productQuery.Where(x => x.Name.Contains(SearchKeyword.Trim()) || x.Description.Contains(SearchKeyword.Trim()));
+
+                //TODO: Sorting, Order By
+
+                //TODO: Create Pagination Parameter DTO
+
+                // Get All Counter
+                int totalCount = productQuery.Count();
+                
+                // Return Data And Counter
+                return Json(new
+                {
+                    totalCount = totalCount,
+                    data = productQuery.Select(x => new
+                    {
+                        Image = x.Image,
+                        Name = x.Name,
+                        Description = x.Description,
+                        Price = x.Price,
+                    }).Skip(skipCount).Take(maxCount).ToList()
+                });
+            }
+            catch (Exception ex)
+            {
+                // When Comes Error then return Error Page 
+
+                // Error Message after redirect page.
+                TempData["Error"] = ex.Message;
+
+
+                //TODO: Setup This page and Gloabal Error Handling.
+                return Redirect("Error");
+            }
         }
     }
 }
